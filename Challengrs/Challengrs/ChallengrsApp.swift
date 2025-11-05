9974f99837ce28648e7ba4a6490fa860bb84f5bc
jameslrivera <1jameslrivera@gmail.com>
2025-11-05 14:01:55 -0500

    Initial Xcode framework and project + README

---
+++ Challengrs/Challengrs/ChallengrsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengrsApp.swift
+//  Challengrs
+//
+//  Created by James Rivera on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ChallengrsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Challengers
+
+MVP: iOS app (SwiftUI) using Firestore for data and Stripe for payments.
+Repo structure:
+- Challengers/        (Xcode project)
+- README.md
+- .gitignore
